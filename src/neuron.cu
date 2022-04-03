76408235a59c63bb4bdccec4d8c650386cb2fd71
Zhengfan Zhang <zhengfaz@ghc50.ghc.andrew.cmu.edu>
2022-04-03 16:46:27 -0400

    basic multi-gamma-cycle implementation; no stdp or wta yet

--- src/neuron.cu
+++ src/neuron.cu
@@ -12,8 +12,14 @@
  * 1 neuron column contains numNeuronPerColumn neurons, output 1 spike on numNeuronPerColumn lines
  * each neuron has numSynapsePerNeuron synapses
  * @param[inout] weight initial synapse weight, and final updated weight array
- * @param[in]    spike_time_in input spike times (numSynapsePerNeuron * numNeuronPerColumn)
- * @param[out]   spike_time_out output spike time (numNeuronPerColumn)
+ * @param[in]    spike_time_in input spike times (numSynapsePerNeuron * numColumns * dataLength)
+ *               x: spike time for each synapse
+ *               y: column ID
+ *               concatenate additional input data samples in the y dimension (additional column IDs)
+ * @param[out]   spike_time_out output spike time (numNeuronPerColumn * numColumns * dataLength)
+ *               rows*cols columns
+                 numNeuronPerColumn spike lines
+                 x: spike time for each neuron (connected to synapse of next layer)
  */
 __global__ void column_kernel(int* weight, int* spike_time_in, int* spike_time_out) {
     // Let each 2d thread block represent a neuron column:
@@ -37,45 +43,56 @@ __global__ void column_kernel(int* weight, int* spike_time_in, int* spike_time_o
 
     // 0 1 3 5 
 
-    // local incStart and incEnd to compare to cycle time
-    int incStart = spike_time_in[/* index here*/];
-    int incEnd = spike_time_in[/* index here*/] + weight[/* index here*/];
-    
-    synapseRNL[threadIdx.x + blockDim.x*threadIdx.y] = false;
-    if (threadIdx.x == 0) {
-        neuronBodyPot[threadIdx.y] = 0;
-    }
-    // TODO sync
-    // end iteration when body potential reaches threshold before gamma cycle time (spikes)
-    // end iteration after cycle time reaches gamma cycle time and no spike
-    for (int tickCycles = 0; tickCycles < gamma_length; ++tickCycles) {
-        // TODO for each synapse, check the spike_time_in to see if start spiking
-        if (tickCycles >= incStart && tickCycles < incEnd) {
-            synapseRNL[threadIdx.x + blockDim.x*threadIdx.y] = true;
-        }
-        else {
-            synapseRNL[threadIdx.x + blockDim.x*threadIdx.y] = false;
-        }
-        // TODO sync
-        // TODO after all RNLs are updated, 1 thread for each neuron updates the body potential
+
+    for (int dataIdx = 0; dataIdx < dataLength; ++dataLength) {
+        // local incStart and incEnd to compare to cycle time
+        int incStart = spike_time_in[/* index here*/];
+        int incEnd = spike_time_in[/* index here*/] + weight[/* index here*/];
+
+        // reset increment indicator
+        synapseRNL[threadIdx.x + blockDim.x*threadIdx.y] = false;
+        
         if (threadIdx.x == 0) {
-            // TODO sum the synapseRNLs for each neuronBodyPot[threadIdx.y]
-            for (int synpaseIdx = 0; synpaseIdx < blockDim.x; ++synpaseIdx) {
-                neuronBodyPot[threadIdx.y] += synapseRNL[threadIdx.x + blockDim.x*threadIdx.y];
-            }
+            // reset body potential for this gama cycle (single dataIdx)
+            neuronBodyPot[threadIdx.y] = 0;
+            // reset spike time to gamma (no spike)
+            spike_time_out[/* index here*/] = gammaLength;
         }
-        // TODO sync
-        if (neuronBodyPot[threadIdx.y] >= spikeThreshold) {
+        __syncthreads();
+        
+        // end iteration when body potential reaches threshold before gamma cycle time (spikes)
+        // end iteration after cycle time reaches gamma cycle time and no spike
+        for (int tickCycles = 0; tickCycles < gammaLength; ++tickCycles) {
+            // for each synapse, check the spike_time_in to see if start spiking
+            if (tickCycles >= incStart && tickCycles < incEnd) {
+                synapseRNL[threadIdx.x + blockDim.x*threadIdx.y] = true;
+            }
+            else {
+                synapseRNL[threadIdx.x + blockDim.x*threadIdx.y] = false;
+            }
+            __syncthreads();
+            // after all RNLs are updated, 1 thread for each neuron updates the body potential
             if (threadIdx.x == 0) {
-                spike_time_out[/* index here*/] = tickCycles;
+                // sum the synapseRNLs for each neuronBodyPot[threadIdx.y]
+                for (int synpaseIdx = 0; synpaseIdx < blockDim.x; ++synpaseIdx) {
+                    neuronBodyPot[threadIdx.y] += synapseRNL[threadIdx.x + blockDim.x*threadIdx.y];
+                }
             }
-            break;
+            __syncthreads();
+            if (neuronBodyPot[threadIdx.y] >= spikeThreshold) {
+                if (threadIdx.x == 0) {
+                    spike_time_out[/* index here*/] = tickCycles;
+                }
+                break;
+            }
         }
+
+
+        // TODO: WTA spike time out
+        
+        // TODO: STDP and update weight
     }
 
-    // TODO: WTA spike time out
-    
-    // TODO: STDP and update weight
 }
 
 void launch_column() {
